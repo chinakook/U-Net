5aacd4fe9b428f5bddd91442ef00d8dc3731038e
chinakook <chinakook@msn.com>
2017-07-21 22:20:44 +0800

    Update unet_test.py

--- unet_test.py
+++ unet_test.py
@@ -9,24 +9,29 @@ from collections import namedtuple
 Batch = namedtuple('Batch', ['data'])
 
 sym, arg_params, aux_params = mx.model.load_checkpoint('u_net', 59)
+all_layers = sym.get_internals()
 
 context=mx.gpu()
-mod = mx.mod.Module(symbol=all_layers['pred_output'], context=context, data_names=['data'], label_names=None)
-mod.bind(for_training=False, data_shapes=[('data', (1, 3, 500, 500))], label_shapes=None, force_rebind=False)
-mod.set_params(arg_params=arg_params, aux_params=aux_params, force_init=False)
+mod = mx.mod.Module(symbol=all_layers['conv_res_output'], context=context, data_names=['data'], label_names=None)
+mod.bind(for_training=False, data_shapes=[('data', (1, 3, 224, 224))], label_shapes=None, force_rebind=False)
+mod.set_params(arg_params=arg_params, aux_params=aux_params, force_init=False, allow_missing=True, allow_extra=True)
 
 im_raw = Image.open('test.png')
 im = np.array(im_raw, dtype=np.float32)
-im = im[np.newaxis,np.newaxis,:,:]
+im = np.transpose(im, (2,0,1))
+im = im[np.newaxis,:,:]
 
 # reshape to process variant image size
 mod.reshape(data_shapes=[('data', (1, 1, im.shape[2], im.shape[3]))])
 mod.forward(Batch([mx.nd.array(im)]), is_train=False)
 
-f = mod.get_outputs()[0].asnumpy()
+f = mx.nd.softmax(mod.get_outputs()[0], axis=1).asnumpy()
 fore_ground = f[0,1,:,:]
-pred_label = (f1>0.5)*255
-pred_label = res.astype(np.uint8)
-res = np.hstack((np.array(im_raw), pred_label))
-plt.imshow(f1>0.5)
+pred_label = (fore_ground>0.5)*255
+pred_label = pred_label.astype(np.uint8)
+
+plt.subplot(121)
+plt.imshow(pred_label>0.5)
+plt.subplot(122)
+plt.imshow(im_raw)
 plt.show()
