7570c329c3ef74bf403321369985843618a15d52
chinakook <chinakook@msn.com>
2017-07-25 17:27:06 +0800

    Update unet_train.py

--- unet_train.py
+++ unet_train.py
@@ -21,6 +21,10 @@ mod.bind(data_shapes=nd_iter.provide_data, label_shapes=nd_iter.provide_label)
 mod.init_params(initializer=mx.init.Xavier(magnitude=2.))
 mod.init_optimizer(optimizer='sgd', optimizer_params={'learning_rate':0.01, 'momentum': 0.9})
 
+# lr_sch = mx.lr_scheduler.MultiFactorScheduler([60 * nd_iter.num_data // nd_iter.batch_size,80 * nd_iter.num_data // nd_iter.batch_size], 0.2)
+# optimizer =mx.optimizer.SGD(momentum = 0.9, learning_rate=0.01, lr_scheduler=lr_sch, rescale_grad=1.0/nd_iter.batch_size)
+# mod.init_optimizer(optimizer=optimizer)
+
 # training...
 mod.fit(nd_iter, num_epoch=60,eval_metric= 'acc')
 
