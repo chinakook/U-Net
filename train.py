a02ae29070961a18811e906460d9d7fd5904deeb
chinakook <chinakook@msn.com>
2018-09-21 16:46:51 +0800

    add dice loss

--- train.py
+++ train.py
@@ -222,7 +222,99 @@ trainer = gluon.Trainer(net.collect_params(), 'sgd', {
     'lr_scheduler': mx.lr_scheduler.PolyScheduler(num_steps * num_epochs, 0.1,  2, 0.0001)
 })
 
-criterion = gluon.loss.SoftmaxCrossEntropyLoss(axis=1)
+from mxnet.gluon.loss import Loss, _apply_weighting, _reshape_like
+
+class WeightedBCEDICE(Loss):
+    r"""Computes the softmax cross entropy loss. (alias: SoftmaxCELoss)
+
+    If `sparse_label` is `True` (default), label should contain integer
+    category indicators:
+
+    .. math::
+
+        \DeclareMathOperator{softmax}{softmax}
+
+        p = \softmax({pred})
+
+        L = -\sum_i \log p_{i,{label}_i}
+
+    `label`'s shape should be `pred`'s shape with the `axis` dimension removed.
+    i.e. for `pred` with shape (1,2,3,4) and `axis = 2`, `label`'s shape should
+    be (1,2,4).
+
+    If `sparse_label` is `False`, `label` should contain probability distribution
+    and `label`'s shape should be the same with `pred`:
+
+    .. math::
+
+        p = \softmax({pred})
+
+        L = -\sum_i \sum_j {label}_j \log p_{ij}
+
+    Parameters
+    ----------
+    axis : int, default -1
+        The axis to sum over when computing softmax and entropy.
+    sparse_label : bool, default True
+        Whether label is an integer array instead of probability distribution.
+    from_logits : bool, default False
+        Whether input is a log probability (usually from log_softmax) instead
+        of unnormalized numbers.
+    weight : float or None
+        Global scalar weight for loss.
+    batch_axis : int, default 0
+        The axis that represents mini-batch.
+
+
+    Inputs:
+        - **pred**: the prediction tensor, where the `batch_axis` dimension
+          ranges over batch size and `axis` dimension ranges over the number
+          of classes.
+        - **label**: the truth tensor. When `sparse_label` is True, `label`'s
+          shape should be `pred`'s shape with the `axis` dimension removed.
+          i.e. for `pred` with shape (1,2,3,4) and `axis = 2`, `label`'s shape
+          should be (1,2,4) and values should be integers between 0 and 2. If
+          `sparse_label` is False, `label`'s shape must be the same as `pred`
+          and values should be floats in the range `[0, 1]`.
+        - **sample_weight**: element-wise weighting tensor. Must be broadcastable
+          to the same shape as label. For example, if label has shape (64, 10)
+          and you want to weigh each sample in the batch separately,
+          sample_weight should have shape (64, 1).
+
+    Outputs:
+        - **loss**: loss tensor with shape (batch_size,). Dimenions other than
+          batch_axis are averaged out.
+    """
+    def __init__(self, axis=-1, sparse_label=True, from_logits=False, weight=None,
+                 batch_axis=0, **kwargs):
+        super(WeightedBCEDICE, self).__init__(weight, batch_axis, **kwargs)
+        self._axis = axis
+        self._sparse_label = sparse_label
+        self._from_logits = from_logits
+
+    def dice_loss(self, F, pred, label):
+        smooth = 1.
+        pred_y = F.argmax(pred, axis = self._axis)
+        intersection = pred_y * label
+        score = (2 * F.mean(intersection, axis=self._batch_axis, exclude=True) + smooth) \
+            / (F.mean(label, axis=self._batch_axis, exclude=True) + F.mean(pred_y, axis=self._batch_axis, exclude=True) + smooth)
+        
+        return - F.log(score)
+
+
+    def hybrid_forward(self, F, pred, label, sample_weight=None):
+        if not self._from_logits:
+            pred = F.log_softmax(pred, self._axis)
+        if self._sparse_label:
+            loss = -F.pick(pred, label, axis=self._axis, keepdims=True)
+        else:
+            label = _reshape_like(F, label, pred)
+            loss = -F.sum(pred*label, axis=self._axis, keepdims=True)
+        loss = _apply_weighting(F, loss, self._weight, sample_weight)
+        diceloss = self.dice_loss(F, pred, label)
+        return F.mean(loss, axis=self._batch_axis, exclude=True) + diceloss
+
+criterion = WeightedBCEDICE(axis=1)
 
 class SegMetric(mx.metric.EvalMetric):
     """CalculSegMetricate metrics for Seg training """
